d24970501149c8156e3be8f26acaf8931fd46ba0
akshaykumark1 <kumarakshay90574@gmail.com>
2025-01-17 13:14:13 +0530

    done

--- amazon/amazon_store/urls.py
+++ amazon/amazon_store/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+
+       
+    path('signin',views.signin,name='signin'),
+    path('signup',views.signup,name='signup'),
+
 ]
