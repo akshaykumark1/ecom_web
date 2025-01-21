888b76d0bde938b9091b3afb7dafa65bf603ae1a
akshaykumark1 <kumarakshay90574@gmail.com>
2025-01-21 13:22:08 +0530

    done

--- amazon/amazon_store/urls.py
+++ amazon/amazon_store/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-
-       
-    # path('signin',views.signin,name='signin'),
-    # path('signup',views.signup,name='signup'),
+    path('signin',views.signin,name='signin'),
+    path('signup',views.signup,name='signup'),
+    path('cart',views.cart,name='cart'),
+    path('orders',views.orders,name='orders'),
+    path('help',views.help,name='help'),
 
 ]
--- amazon/amazon_store/views.py
+++ amazon/amazon_store/views.py
@@ -6,3 +6,16 @@ def home(request):
     
     
     
+def signin(request):
+    return render(request, 'signin.html')
+
+def signup(request):
+    return render(request, 'signup.html')    
+
+def cart(request):
+    return render(request, 'cart.html')    
+
+def orders(request):
+    return render(request, 'orders.html')    
+def help(request):
+    return render(request, 'help.html')    
